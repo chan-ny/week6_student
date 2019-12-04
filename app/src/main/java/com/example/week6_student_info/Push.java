5bcf9a58511a4bdab28cba675c6cb31053f45bf7
chan <demony234@gmail.com>
2019-12-04 17:30:09 +0700

    add activity PUSH

---
+++ app/src/main/java/com/example/week6_student_info/Push.java
@@ -0,0 +1,35 @@
+package com.example.week6_student_info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Push extends AppCompatActivity {
+    TextView textView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push);
+        String[] arr = new String[4];
+        String id,name,department,subject,year;
+
+        Intent intent=getIntent();
+
+        id=intent.getStringExtra("id");
+        name = intent.getStringExtra("name");
+        department= intent.getStringExtra("deparment");
+        subject = intent.getStringExtra("subject");
+        year = intent.getStringExtra("year");
+
+        textView1 = findViewById(R.id.textView);
+        textView1.setText("code \n"+ id +" name \n"+name+ " department \n"+department+ " subject \n and "+subject+" year "+year);
+
+
+
+    }
+
+}
